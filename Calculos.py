37028ecd90082a09b9b1133ac1ddd84e2fe2c208
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-01-11 14:42:27 -0300

    CriacÃção da classe cálculos

---
+++ Calculos.py
@@ -0,0 +1,14 @@
+#ano_atual = 2022
+#ano_fabricacao = 2003
+#km_total = 44410.0
+
+# outra forma de declarar variáveis
+ano_atual,ano_fabricacao,km_total = 2022,2003,44410.0
+
+km_media = km_total / (ano_atual - ano_fabricacao)
+
+km_total += km_media
+
+print(km_total)
+
+#print(km_media)
